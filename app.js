c8bbc623a8df5c08332fc4baafd58e2ff70d7a32
dave <oquiladave@gmail.com>
2024-10-03 16:13:46 +0800

    commitedd

--- app.js
+++ app.js
@@ -11,3 +11,18 @@ $(document).ready(function(){
 import { Ripple, initMDB } from "mdb-ui-kit";
 
 initMDB({ Ripple });
+
+
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      responsive: {
+          0: { items: 1 },
+          600: { items: 2 },
+          1000: { items: 3 }
+      }
+  });
+});
