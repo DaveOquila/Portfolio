17d94fbb8c98542dc05f23c28f8781a41d3bc51a
dave <oquiladave@gmail.com>
2024-10-03 17:21:54 +0800

    lets goo

--- app.js
+++ app.js
@@ -5,9 +5,3 @@ $(document).ready(function(){
        $("#imgmodal").modal('show');
   });
 });
-
-
-// Initialization for ES Users
-import { Ripple, initMDB } from "mdb-ui-kit";
-
-initMDB({ Ripple });
