494715e299cd89efcd9c8915ce4ee4118ef02a76
dave <oquiladave@gmail.com>
2024-10-03 16:19:49 +0800

    going Back

--- app.js
+++ app.js
@@ -11,18 +11,3 @@ $(document).ready(function(){
 import { Ripple, initMDB } from "mdb-ui-kit";
 
 initMDB({ Ripple });
-
-
-
-$(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-      loop: true,
-      margin: 10,
-      nav: true,
-      responsive: {
-          0: { items: 1 },
-          600: { items: 2 },
-          1000: { items: 3 }
-      }
-  });
-});
