cbce3ddd7ed45c69a83f6d01a0cadf0c1ab3fcb4
dave <oquiladave@gmail.com>
2024-10-03 11:37:01 +0800

    new commit

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-const elementsToFadeInUpOnScroll = document.querySelectorAll(".fade-in-up-on-scroll");
-if (elementsToFadeInUpOnScroll) {
-  window.addEventListener("scroll", function(event) {
-    elementsToFadeInUpOnScroll.forEach(function(element) {
-      if (window.scrollY >= (element.offsetTop - window.innerHeight)) {
-        element.classList.add("fade-in-up");
-      } else {
-        element.classList.remove("fade-in-up");
-      }
-    });
+$(document).ready(function(){
+  $("img").click(function(){
+     var img=$(this).attr('src');
+       $("#show-img").attr('src',img);
+       $("#imgmodal").modal('show');
   });
-}
+});
